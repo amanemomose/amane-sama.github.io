4e7ef0a94a530b68f4c8e63b03e8368939fe046a
amanemomose <148068197+amanemomose@users.noreply.github.com>
2023-10-16 19:21:18 -0300

    Update 2018-02-26.js

--- src/js/data/2018-02-26.js
+++ src/js/data/2018-02-26.js
@@ -27,12 +27,7 @@ dataSet[dataSetVersion].options = [
       { name: "DS Side Characters", key: "ds" },
       { name: "Puchimas!", key: "puc" },
       { name: "Leon", key: "leon" }
-{
-  name: 'Filter Lolis',
-  key: 'loli',
-  tooltip: 'Check this if you want to remove lolis from being listed.'
-  checked: true,
-      }
+      { name: 'Filter Lolis', key: 'loli', tooltip: 'Check this if you want to remove lolis from being listed.' checked: true,}
     ]
   }
 ];
